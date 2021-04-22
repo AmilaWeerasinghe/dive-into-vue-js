326b3b5898a1074147f2ef7f48ba04f928ce9686
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-04-22 23:00:29 +0530

    two way binding

--- blog/src/components/Home.vue
+++ blog/src/components/Home.vue
@@ -3,6 +3,12 @@
 <h1>Name:{{name}}</h1>
 <h1>Email:{{getName()}}</h1>
 <h1>Email:{{getData().email}}</h1>
+<h1>{{count}}</h1>
+<input type="text" v-model="data"/>
+<h2>two way binding :{{data}}</h2>
+
+<button v-on:click="getalert()">Click</button>
+<button v-on:click="increase()">Click Count</button>
 </template>
 
 <script>
@@ -11,7 +17,9 @@ export default{
     data(){
         return{
             name:"amila",
-            email:"amila@arimaclanka"
+            email:"amila@arimaclanka",
+            count:0,
+            data:0
         }
     },
     methods:{
@@ -23,6 +31,12 @@ export default{
                 name:"anil",
                 email:"sidhu"
             }
+        },
+        getalert(){
+            alert("button clicked")
+        },
+        increase(){
+            this.count=this.count+1
         }
     }
 }
