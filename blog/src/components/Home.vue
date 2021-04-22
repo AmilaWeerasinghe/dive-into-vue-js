f44e2aceb832752b77e796209f6dc3cfe2fa893f
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-04-22 22:35:11 +0530

    interpolation

--- blog/src/components/Home.vue
+++ blog/src/components/Home.vue
@@ -1,10 +1,30 @@
 <template>
 <h1>Home Component</h1>
+<h1>Name:{{name}}</h1>
+<h1>Email:{{getName()}}</h1>
+<h1>Email:{{getData().email}}</h1>
 </template>
 
 <script>
 export default{
-    name:"Home"
+    name:"Home",
+    data(){
+        return{
+            name:"amila",
+            email:"amila@arimaclanka"
+        }
+    },
+    methods:{
+        getName(){
+            return "Amila@gmail"
+        },
+        getData(){
+            return{
+                name:"anil",
+                email:"sidhu"
+            }
+        }
+    }
 }
 </script>
 
