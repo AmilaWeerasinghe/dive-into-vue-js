cacd85da414ffb1e85c5021e3a96deaee76b8677
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-04-24 21:37:46 +0530

    data and functions from parent to child components

--- blog/src/components/Home.vue
+++ blog/src/components/Home.vue
@@ -1,70 +1,88 @@
 <template>
-<Child :name="name" :getName="getName"/>
-<h1>Home Component</h1>
-<h2>Name:{{name}}</h2>
-<h2>Email:{{getName()}}</h2>
-<h3>Email:{{getData().email}}</h3>
-<h4>{{count}}</h4>
-<input type="text" v-model="data" />
-<h4>two way binding :{{data}}</h4>
+  <Child :name="name" :getName="getName" />
+  <h1>Home Component</h1>
+  <h2>Name:{{ name }}</h2>
+  <h2>Email:{{ getName() }}</h2>
+  <h3>Email:{{ getData().email }}</h3>
+  <h4>{{ count }}</h4>
+  <input type="text" v-model="data" />
+  <h4>two way binding :{{ data }}</h4>
 
-<button v-on:click="getalert()">Click</button>
-<button v-on:click="increase()">Click Count</button>
-<h3 v-if="show">Condition in vue is true</h3>
-<h3 v-else>Condition in vue is false</h3>
-<button v-on:click="show=!show">Click to change the condition</button>
-<ul>
-<li v-for="item in technology" :key="item">
-{{item}}
-</li>
-</ul>
-
+  <button v-on:click="getalert()">Click</button>
+  <button v-on:click="increase()">Click Count</button>
+  <h3 v-if="show">Condition in vue is true</h3>
+  <h3 v-else>Condition in vue is false</h3>
+  <button v-on:click="show = !show">Click to change the condition</button>
+  <ul>
+    <li v-for="item in technology" :key="item">
+      {{ item }}
+    </li>
+  </ul>
+  <ul>
+    <li v-for="item in users" :key="item.name">
+      <User :data="item" :getalertUser="getalertUser" />
+    </li>
+  </ul>
 </template>
 
 <script>
-import Child from './Child.vue'
+import Child from "./Child.vue";
+import User from "./User.vue";
 export default {
-    name: "Home",
-    data() {
-        return {
-            name: "amila",
-            email: "amila@arimaclanka",
-            count: 0,
-            data: 0,
-            show: true,
-            technology: [
-                "java", 
-                "html", 
-                "c", 
-                "javascript"
-                ]
-        }
+  name: "Home",
+  data() {
+    return {
+      name: "amila",
+      email: "amila@arimaclanka",
+      count: 0,
+      data: 0,
+      show: true,
+      technology: ["java", "html", "c", "javascript"],
+      users: [
+        {
+          name: "Amila",
+          email: "amila@gmail",
+        },
+        {
+          name: "Kanthi",
+          email: "k@gmail",
+        },
+        {
+          name: "Nimal",
+          email: "Nimal@gmail",
+        },
+      ],
+    };
+  },
+  components: {
+    Child,
+    User,
+  },
+  methods: {
+    getName() {
+      return "Amila@gmail";
     },
-    components:{
-        Child
+    getData() {
+      return {
+        name: "anil",
+        email: "sidhu",
+      };
     },
-    methods: {
-        getName() {
-            return "Amila@gmail"
-        },
-        getData() {
-            return {
-                name: "anil",
-                email: "sidhu"
-            }
-        },
-        getalert() {
-            alert("button clicked")
-        },
-        increase() {
-            this.count = this.count + 1
-        }
-    }
-}
+    getalert() {
+      alert("button clicked");
+    },
+    getalertUser(name) {
+      alert(name);
+    },
+    increase() {
+      this.count = this.count + 1;
+    },
+  },
+};
 </script>
 
 <style scoped>
 h1 {
-    color: orange
+  color: orange;
 }
 </style>
---
+++ blog/src/components/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>{{ data.name }}</h3>
+  <button v-on:click="getalertUser(data.name)">alert name</button>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: {
+    data: Object,
+    getalertUser: Function,
+  },
+};
+</script>
