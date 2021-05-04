de83e8402427e774883c7ed63f731ae10aa71c97
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-05-04 13:12:44 +0530

    blockly vue

---
+++ blockly/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <first-component></first-component>
+  </div>
+</template>
+
+<script>
+import FirstComponent from "./components/FirstComponent.vue";
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  components: {
+    FirstComponent,
+  },
+};
+</script>
+
+<style>
+</style>
