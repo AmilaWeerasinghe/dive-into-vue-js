50a4887475d4cd17c53bcc666da9f1a31acc21c9
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-05-10 16:41:07 +0530

    bootstrp 4

--- blockly/src/App.vue
+++ blockly/src/App.vue
@@ -1,27 +1,20 @@
 \<template>
   <div id="app">
     <FirstComponent></FirstComponent>
-    <div id="blocklyDiv" style="height: 480px; width: 600px;"></div>
-    <xml id="toolbox" style="display: none">
-      <block type="controls_if"></block>
-      <block type="controls_repeat_ext"></block>
-      <block type="logic_compare"></block>
-      <block type="math_number"></block>
-      <block type="math_arithmetic"></block>
-      <block type="text"></block>
-      <block type="text_print"></block>
-    </xml>
+    <boot-stap-component></boot-stap-component>
   </div>
 </template>
 
 <script>
 import FirstComponent from "./components/FirstComponent.vue";
+import BootStapComponent from "./components/BootStapComponent.vue";
 import Blockly from "blockly";
 
 export default {
   name: "App",
   components: {
     FirstComponent,
+    BootStapComponent,
   },
 };
 </script>
